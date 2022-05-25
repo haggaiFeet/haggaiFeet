03f77d65d49b578f1619e94922a4814b28df084b
haggaiFeet <105278591+haggaiFeet@users.noreply.github.com>
2022-05-25 17:39:14 +0200

    Add files via upload

---
+++ Discord-Twitter-role/twitterCredentialsPrivate.example.py
@@ -0,0 +1,5 @@
+
+consumer_key =' nzYNfRUY6HhpL8J8u2DAbMu3g'
+consumer_secret = 'BCi1jnhp0lBCcKv3Qfa9KXkDgxsFVREnZKZeYWZD6RY5IqsTFA'
+access_token = '1498298065681367041-jsngy3idFjlSGw7iCkag6qI7HLwnES'
+access_token_secret = 'Gl4gFMpasgS7H0tTGvfg3Lky64uT2kx2KVTwDokLOczV6'
